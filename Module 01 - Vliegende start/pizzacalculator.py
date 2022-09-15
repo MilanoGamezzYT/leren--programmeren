b663996b8db26211816be33ced9a791d7bdf53fa
Milan Sebes <99067157@mydavinci.nl>
2022-09-15 11:10:19 +0200

    test

---
+++ Module 01 - Vliegende start/pizzacalculator.py
@@ -0,0 +1,26 @@
+# Milan Sebes Pizza Calculator
+
+Small = 10
+Medium = 12
+Large = 15
+afmeting_small = 25
+afmeting_medium = 29
+afmeting_large = 35
+
+
+s = int(input('Hoeveel Small pizzas (25cm) (€10) wilt u hebben? '))
+totaal_small = s * 10
+print(f'€{totaal_small}')
+
+m = int(input('Hoeveel Medium pizzas (29cm) (€12) wilt u hebben?  '))
+totaal_medium = m * 12
+print(f'€{totaal_medium}')
+
+l = int(input('Hoeveel Large pizzas (35cm) (€15) wilt u hebben?  '))
+totaal_large = l * 15
+print(f'€{totaal_large}') 
+
+print(f'€{totaal_small + totaal_medium + totaal_large} totaal')
+print("Eetsmakelijk!")
+
+
