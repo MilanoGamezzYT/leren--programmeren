e3192c6a5f7fbe57cb909806b0d1cbdb237d95af
Milan Sebes <99067157@mydavinci.nl>
2022-09-20 11:50:39 +0200

    test

--- Module 01 - Vliegende start/pizzacalculator.py
+++ Module 01 - Vliegende start/pizzacalculator.py
@@ -5,16 +5,16 @@ Medium = 12
 Large = 15
 
 
-s = int(input('Hoeveel Small pizzas (25cm) (€10) wilt u hebben? '))
-totaal_small = s * 10
+s = int(input('Hoeveel Small pizzas wilt u hebben? '))
+totaal_small = s * Small
 print(f'€{totaal_small}')
 
-m = int(input('Hoeveel Medium pizzas (29cm) (€12) wilt u hebben?  '))
-totaal_medium = m * 12
+m = int(input('Hoeveel Medium pizzas wilt u hebben?  '))
+totaal_medium = m * Medium
 print(f'€{totaal_medium}')
 
-l = int(input('Hoeveel Large pizzas (35cm) (€15) wilt u hebben?  '))
-totaal_large = l * 15
+l = int(input('Hoeveel Large pizzas wilt u hebben?  '))
+totaal_large = l * Large
 print(f'€{totaal_large}') 
 
 print(f'€{totaal_small + totaal_medium + totaal_large} totaal')
