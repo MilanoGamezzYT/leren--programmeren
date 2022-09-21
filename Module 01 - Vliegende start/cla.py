888fe725b094c223b4c4554f888bf5665c8a4b42
Milan Sebes <99067157@mydavinci.nl>
2022-09-21 14:13:00 +0200

    test

---
+++ Module 01 - Vliegende start/cla.py
@@ -0,0 +1,9 @@
+cijfer = int(input("Voer een getal in  "))
+
+if cijfer <= 6:
+    print("Waarschuwing - onvoldoende!")
+elif cijfer < 4:
+    print("Stuur Waarschuwing ouders")
+
+   
+
--- Module 01 - Vliegende start/feestlunchcent.py
+++ Module 01 - Vliegende start/feestlunchcent.py
@@ -1,22 +1,26 @@
 croissantjes = 17
-croissantjesprijs = 0.39 
+croissantjesprijs = 39 
 stokbrood = 2
-stokbroodprijs = 2.78
+stokbroodprijs = 278
 kortingsbon = 3
-kortingsprijs = 0.50
+kortingsprijs = 150
 totaal = croissantjes * croissantjesprijs
 totaal2 = stokbrood * stokbroodprijs
 totaal3 = kortingsbon * kortingsprijs
 
 x = int(input('Hoeveel croissantjes wilt u?  '))
 totaal_croissant = x * 39
-print(f'{totaal_croissant} cent')
+print(f'dat kost {totaal_croissant} cent')
+print(" ")
 
 y = int(input('Hoeveel stokbroden wilt u?    '))
 totaal_stokbrood = y * 278
-print(f'{totaal_stokbrood} cent')
+print(f'dat kost {totaal_stokbrood} cent')
+print(" ")
 
 w = int(input('Hoeveel kortingsbonnen heeft u?  '))
 totaal_kortingsbonnen = w * 50
+print(" ")
 
-print(f'{totaal_croissant + totaal_stokbrood - totaal_kortingsbonnen} cent')
+print(f'Dus de prijs voor de feestlunch kost {totaal_croissant + totaal_stokbrood - totaal_kortingsbonnen} cent')
+print("Omgerekent is dat €10.69")
