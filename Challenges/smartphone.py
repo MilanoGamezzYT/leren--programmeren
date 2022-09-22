86ccca524c1d6d791ecee9c9b7a04b03c3a13925
Milan Sebes <99067157@mydavinci.nl>
2022-09-22 10:16:33 +0200

    smartphone

--- Challenges/smartphone.py
+++ Challenges/smartphone.py
@@ -1,14 +1,20 @@
+print('Tussen welke telefoons twijfel je?')
+x = input()
+print('Twijfel = ' + x)
+y = input()
+print('en de ' + y)
+
 
 
 prijs_iphone = int(input('Hoe duur is de iphone 13? '))
 print(f'De Iphone kost €{prijs_iphone}')
 prijs_samsung = int(input('Hoe duur is de Samsung? '))
-print(f'De samsung kost €{prijs_samsung}')
+print(f'De {y} kost €{prijs_samsung}')
 print("")
-print(f'De Iphone is het duurst, de telefoon kost: €{prijs_iphone}')
-print(f'De samsung is het goedkoopst, de telefoon kost: €{prijs_samsung} ')
+print(f'De {x} is het duurst, de telefoon kost: €{prijs_iphone}')
+print(f'De {y} is het goedkoopst, de telefoon kost: €{prijs_samsung} ')
 print("")
-print(f'Het advies is dus om de Iphone te kopen. Deze is namelijk {prijs_iphone - prijs_samsung} euro duurder dan de Samsung')
+print(f'Het advies is dus om de {x} te kopen. Deze is namelijk {prijs_iphone - prijs_samsung} euro duurder dan de {y}')
 
 
 
