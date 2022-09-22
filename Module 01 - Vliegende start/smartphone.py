7ee6c28c0107d399966c78d01e2a93b9bab67f29
Milan Sebes <99067157@mydavinci.nl>
2022-09-22 09:50:02 +0200

    smartphone

---
+++ Module 01 - Vliegende start/smartphone.py
@@ -0,0 +1,14 @@
+Verschil = 40
+
+prijs_iphone = int(input('Hoe duur is de iphone 13? '))
+print(f'De Iphone kost €{prijs_iphone}')
+prijs_samsung = int(input('Hoe duur is de Samsung? '))
+print(f'De samsung kost €{prijs_samsung}')
+print("")
+print(f'De Iphone is het duurst, de telefoon kost: €{prijs_iphone}')
+print(f'De samsung is het goedkoopst, de telefoon kost: €{prijs_samsung} ')
+print("")
+print(f'Het advies is dus om de Iphone te kopen. Deze is namelijk {Verschil} euro duurder dan de Samsung')
+
+
+
