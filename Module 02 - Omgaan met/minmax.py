d341af5d3e84226669bf334566e70827506ea7f6
Milan Sebes <99067157@mydavinci.nl>
2022-09-23 09:34:07 +0200

    elif-statement

--- Module 02 - Omgaan met/minmax.py
+++ Module 02 - Omgaan met/minmax.py
@@ -1,12 +1,13 @@
 a = int(input('Voer een getal in '))
 b = int(input('Voer nog een getal in '))
-if a > b:
-    print('a is het grootste getal')
-elif b > a: 
-    print('a is niet het grootste getal')
 
-    a = max
-    b = min
+if b > a: 
+    print('a is het kleinste getal')
+elif a > b:
+    print('a is het grootste getal')
+    
+    a = min
+    b = max
 
 
 
