4efabdcbbbc2630fffd015cabd120adcd68eed57
Milan Sebes <99067157@mydavinci.nl>
2022-09-23 09:30:34 +0200

    input en if-statement

--- Module 02 - Omgaan met/minmax.py
+++ Module 02 - Omgaan met/minmax.py
@@ -2,9 +2,11 @@ a = int(input('Voer een getal in '))
 b = int(input('Voer nog een getal in '))
 if a > b:
     print('a is het grootste getal')
-    
-a = max
-b = min
+elif b > a: 
+    print('a is niet het grootste getal')
+
+    a = max
+    b = min
 
 
 
