1dbe0a41aac84bbdd6ef7603b66c977a5949ca98
Milan Sebes <99067157@mydavinci.nl>
2022-09-29 15:20:05 +0200

    deel 2

--- Module 02 - Omgaan met/sollicitatie.py
+++ Module 02 - Omgaan met/sollicitatie.py
@@ -3,19 +3,19 @@ geslacht = input('Wat is uw geslacht? ')
 if geslacht ==  ('man'): 
     snor_vraag = input('Heeft u een snor? ')
     if snor_vraag == 'ja':
-        snor_breedte = int(input('Hoe breed is uw snor in cm? '))
+        snor_breedte = int(input('Hoe breed is uw snor in cm?  '))
 if geslacht == ('vrouw'):
-    haar_kleur = input('Wat voor kleur haar heeft u? ')
-    haar_lengte = int(input('Hoelang is uw haar in cm? '))
+    haar_kleur = input('Wat voor kleur haar heeft u?  ')
+    haar_lengte = int(input('Hoelang is uw haar in cm?  '))
 hoge_hoed = input('Heeft u een hoge hoed? ')
-rijbewijs = input('heeft u een geldig vrachtwagenbewijs?')
+rijbewijs = input('heeft u een geldig vrachtwagenbewijs? ')
 lengte = int(input('Hoelang bent u in cm?' ))
 gewicht = int(input('Hoeveel weegt u in kg? '))
-diploma = input('Heeft u een Diploma MBO-4 ondernemen?' )
-certificaat = input('Heeft u een Certificaat “Overleven met gevaarlijk personeel?' )
-dieren_dressuur = int(input('Hoeveel jaar praktijkervaring heeft u met dieren-dressuur?' ))
-jongleren = int(input('Hoeveel jaar ervaring heeft u met jongleren?' ))
-acrobatiek = int(input('Hoeveel jaar praktijkervaring heeft u met acrobatiek?' ))
+diploma = input('Heeft u een Diploma MBO-4 ondernemen? ')
+certificaat = input('Heeft u een Certificaat “Overleven met gevaarlijk personeel? ' )
+dieren_dressuur = int(input('Hoeveel jaar praktijkervaring heeft u met dieren-dressuur? ' ))
+jongleren = int(input('Hoeveel jaar ervaring heeft u met jongleren? ' ))
+acrobatiek = int(input('Hoeveel jaar praktijkervaring heeft u met acrobatiek? ' ))
 
 
 if (geslacht == 'man' and snor_vraag == 'ja' and snor_breedte >= 10) or (geslacht == 'vrouw' and haar_kleur == 'rood' and haar_lengte >= 20) and hoge_hoed == 'ja' and rijbewijs == 'ja' and lengte >= 150 and gewicht >= 90 and diploma == 'ja' and certificaat == 'ja' and (dieren_dressuur >= 4 or jongleren >= 5 or acrobatiek >= 3):
