4090e919a431d5fed0236dafdc201f08267be8a0
Milan Sebes <99067157@mydavinci.nl>
2022-09-29 09:50:07 +0200

    deel 2

--- Module 02 - Omgaan met/sollicitatie.py
+++ Module 02 - Omgaan met/sollicitatie.py
@@ -1,28 +1,25 @@
-Naam = input('Wat is uw naam?')
-Gender = input('Wat is uw gender')
-if Gender ==  ('man'):
+naam = input('Wat is uw naam?')
+geslacht = input('Wat is uw gender')
+if geslacht ==  ('man'):
     snor1 = input('Heeft u een snor?')
     if snor1 == 'ja':
-        snor2 = int(input('Hoe breed is je snor in cm?'))
-if Gender == ('vrouw'):
-    Kapsel1 = input('Wat voor soort haar heeft u?')
-    Kapsel2 = input('Wat voor kleur haar heeft u?')
-    Kapsel3 = int(input('Hoelang is uw haar in cm?'))
-hoed = input("Heeft u een hoge hoed?")
-Rijbewijs = input("heeft u een geldig vrachtwagenbewijs?")
-lengte = int(input("Hoelang bent u?"))
-gewicht = int(input('Hoeveel weegt u?'))
-MBO = input('Bent u in bezit van een Diploma MBO-4 ondernemen?')
-Certificaat = input('Heeft Certificaat “Overleven met gevaarlijk personeel?')
+        snor2 = int(input('Hoe breed is uw snor in cm?'))
+if geslacht == ('vrouw'):
+    kapsel1 = input('Wat voor soort haar heeft u?')
+    kapsel2 = input('Wat voor kleur haar heeft u?')
+    kapsel3 = int(input('Hoelang is uw haar in cm?'))
+hoge_hoed = input("Heeft u een hoge hoed?")
+rijbewijs = input("heeft u een geldig vrachtwagenbewijs?")
+lengte = int(input("Hoelang bent u in m?"))
+gewicht = int(input('Hoeveel weegt u in kg?'))
+diploma = input('Bezit u een Diploma MBO-4 ondernemen?')
+Certificaat = input('Heeft u een Certificaat “Overleven met gevaarlijk personeel?')
 Dieren_dressuur = int(input('Hoeveel jaar praktijkervaring heeft u met dieren-dressuur?'))
 jongleren = int(input('Hoeveel jaar ervaring heeft u met jongleren?'))
 acrobatiek = int(input('Hoeveel jaar praktijkervaring heeft u met acrobatiek?'))
-strafblad = input('Heeft/had u een strafblad?')
-banaan = input('Houd u van banaan?')
-games = int(input('Hoeveel games speelt u?'))
-familie = int(input('Hoeveel broers en/of zussen heeft u?'))
 
-if (Gender == 'man') or(Gender == 'vrouw' and Kapsel1 == 'krullen' and Kapsel2 == 'rood' and Kapsel3 >= 20) and  hoed == 'ja' and Rijbewijs == 'ja' and lengte >= 150 and gewicht >= 90 and MBO == 'ja' and Certificaat == 'ja' and Dieren_dressuur >= 4 or jongleren >= 5 or acrobatiek >= 3 and strafblad == 'ja' and banaan == 'nee' and games >= 4 and familie >= 1:
-    print('Je bent geslaagd')
+
+if (geslacht == 'man') or(geslacht == 'vrouw' and kapsel1 == 'krullen' and kapsel2 == 'rood' and kapsel3 >= 20) and hoge_hoed == 'ja' and rijbewijs == 'ja' and lengte >= 150 and gewicht >= 90 and diploma == 'ja' and Certificaat == 'ja' and Dieren_dressuur >= 4 or jongleren >= 5 or acrobatiek >= 3:
+    print('Gefeliciteerd, u bent aangenomen!')
 else:
-    print('Je hebt de sollicitaie niet goed ingevuld.')
+    print('U bent helaas niet aangenomen.')
