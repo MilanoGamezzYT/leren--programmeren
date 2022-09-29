70a8ec650ac4ba82865867c4e7f1b080bd0eb0e4
Milan Sebes <99067157@mydavinci.nl>
2022-09-29 11:16:40 +0200

    deel 2

--- Module 02 - Omgaan met/sollicitatie.py
+++ Module 02 - Omgaan met/sollicitatie.py
@@ -1,9 +1,9 @@
 naam = input('Wat is uw naam?' )
 geslacht = input('Wat is uw geslacht? ')
 if geslacht ==  ('man'): 
-    snor1 = input('Heeft u een snor? ')
-    if snor1 == 'ja':
-        snor2 = int(input('Hoe breed is uw snor in cm? '))
+    snor_vraag = input('Heeft u een snor? ')
+    if snor_vraag == 'ja':
+        snor_breedte = int(input('Hoe breed is uw snor in cm? '))
 if geslacht == ('vrouw'):
     haar_kleur = input('Wat voor kleur haar heeft u? ')
     haar_lengte = int(input('Hoelang is uw haar in cm? '))
@@ -18,7 +18,7 @@ jongleren = int(input('Hoeveel jaar ervaring heeft u met jongleren?' ))
 acrobatiek = int(input('Hoeveel jaar praktijkervaring heeft u met acrobatiek?' ))
 
 
-if (geslacht == 'man' and snor1 == 'ja' and snor2 >= 10) or (geslacht == 'vrouw' and haar_kleur == 'rood' and haar_lengte >= 20) and hoge_hoed == 'ja' and rijbewijs == 'ja' and lengte >= 150 and gewicht >= 90 and diploma == 'ja' and certificaat == 'ja' and (dieren_dressuur >= 4 or jongleren >= 5 or acrobatiek >= 3):
+if (geslacht == 'man' and snor_vraag == 'ja' and snor_breedte >= 10) or (geslacht == 'vrouw' and haar_kleur == 'rood' and haar_lengte >= 20) and hoge_hoed == 'ja' and rijbewijs == 'ja' and lengte >= 150 and gewicht >= 90 and diploma == 'ja' and certificaat == 'ja' and (dieren_dressuur >= 4 or jongleren >= 5 or acrobatiek >= 3):
     print('Gefeliciteerd, u bent aangenomen! ')
 else:
     print('U bent niet aangenomen ')
