d0bd52294eaec19cbc83ecd31057fa981c58f6b0
Milan Sebes <99067157@mydavinci.nl>
2022-09-30 09:29:12 +0200

    deel 2

--- Module 02 - Omgaan met/sollicitatie.py
+++ Module 02 - Omgaan met/sollicitatie.py
@@ -1,24 +1,25 @@
-naam = input('Wat is uw naam? ')
-geslacht = input('Wat is uw geslacht? ')
+naam = input('Wat is uw naam? ').lower
+geslacht = input('Wat is uw geslacht? ').lower
 if geslacht ==  ('man'): 
-    snor_vraag = input('Heeft u een snor? ')
+    snor_vraag = input('Heeft u een snor? ').lower
     if snor_vraag == 'ja':
         snor_breedte = int(input('Hoe breed is uw snor in cm?  '))
 if geslacht == ('vrouw'):
-    haar_kleur = input('Wat voor kleur haar heeft u?  ')
-    haar_lengte = int(input('Hoelang is uw haar in cm?  '))
-hoge_hoed = input('Heeft u een hoge hoed? ')
-rijbewijs = input('heeft u een geldig vrachtwagenbewijs? ')
+    haar_kleur = input('Wat voor kleur haar heeft u?  ').lower
+    if haar_kleur == 'rood':
+        haar_lengte = int(input('Hoelang is uw haar in cm?  '))
+hoge_hoed = input('Heeft u een hoge hoed? ').lower
+rijbewijs = input('heeft u een geldig vrachtwagenbewijs? ').lower
 lengte = int(input('Hoelang bent u in cm?' ))
 gewicht = int(input('Hoeveel weegt u in kg? '))
-diploma = input('Heeft u een Diploma MBO-4 ondernemen? ')
-certificaat = input('Heeft u een certificaat genaamd Overleven met gevaarlijk personeel? ' )
+diploma = input('Heeft u een Diploma MBO-4 ondernemen? ').lower
+certificaat = input('Heeft u een certificaat genaamd Overleven met gevaarlijk personeel? ' ).lower
 dieren_dressuur = int(input('Hoeveel jaar praktijkervaring heeft u met dieren-dressuur? ' ))
 jongleren = int(input('Hoeveel jaar ervaring heeft u met jongleren? ' ))
 acrobatiek = int(input('Hoeveel jaar praktijkervaring heeft u met acrobatiek? ' ))
 print("")
 
-if (geslacht == 'man' and snor_vraag == 'ja' and snor_breedte >= 10) or (geslacht == 'vrouw' and haar_kleur == 'rood' and haar_lengte >= 20) and hoge_hoed == 'ja' and rijbewijs == 'ja' and lengte >= 150 and gewicht >= 90 and diploma == 'ja' and certificaat == 'ja' and (dieren_dressuur >= 4 or jongleren >= 5 or acrobatiek >= 3):
+if ((geslacht == 'man' and snor_vraag == 'ja' and snor_breedte >= 10) or (geslacht == 'vrouw' and haar_kleur == 'rood' and haar_lengte >= 20)) and hoge_hoed == 'ja' and rijbewijs == 'ja' and lengte >= 150 and gewicht >= 90 and diploma == 'ja' and certificaat == 'ja' and (dieren_dressuur >= 4 or jongleren >= 5 or acrobatiek >= 3):
     print(f'Gefeliciteerd {naam}, u bent aangenomen!')
 else:
     print('U bent niet aangenomen omdat u niet aan de eisen voldoet. ')
