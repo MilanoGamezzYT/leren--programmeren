3fecc5a37abae6ac3d8b6202943645f8feff2b64
Milan Sebes <99067157@mydavinci.nl>
2022-10-13 14:28:44 +0200

    game

---
+++ Module 02 - Omgaan met/game.py
@@ -0,0 +1,25 @@
+game_name = "Twilight Princess 2"
+
+print(f'Welcome to {game_name}!')
+start_game = input('do you want to start the game?')
+if start_game == 'yes':
+    print('starting game...')
+    print('You wake up and you heard some noices')
+    print('You go see whats outside')
+    print('You see some chilren playing')
+    print('You go to the children')
+    print('You ask them what they are doing')
+    print('They say they are playing')
+    slingshot = str(print('They ask you if you can find the slingshot'))
+    if slingshot == 'yes':
+        print('You are walking to the main village')
+        print('You see a shop')
+        print('You go to the shop')
+        print('You ask the shopkeeper if he has a slingshot')
+        print('He says yes but you need to do some quests')
+        print('You say ok')
+        print('The first quest is to catch a fish for the cat')
+        print('Then an man asks if you can help him')   
+        print('You say yes') 
+
+
