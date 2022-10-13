883a041bc31d91550a6c12d843ede7914ae0184a
Milan Sebes <99067157@mydavinci.nl>
2022-10-13 09:36:03 +0200

    zwembad

--- Challenges/zwembad.py
+++ Challenges/zwembad.py
@@ -4,3 +4,11 @@ diepte = 1.5
 
 grond = lengte * breedte * diepte
 print(f'{grond}')
+
+uitgraven = 25
+afvoer_grond = 32.50
+totaal_uitgraven = uitgraven * grond
+totaal_afvoer = afvoer_grond * grond 
+offerte = totaal_afvoer + totaal_uitgraven
+print(f'Offerte voor een zwembad van 8 bij 3 bij 1.5 meter, inhoud = {grond} m3')
+print(f'{totaal_afvoer} + {totaal_uitgraven} = {offerte}')
