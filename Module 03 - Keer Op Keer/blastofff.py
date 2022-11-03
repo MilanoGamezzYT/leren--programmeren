4c51c9c8e0619fed87c6f61310fa9fa80f4e4a1a
Milan Sebes <99067157@mydavinci.nl>
2022-11-03 12:54:32 +0100

    tijd

--- Module 03 - Keer Op Keer/blastofff.py
+++ Module 03 - Keer Op Keer/blastofff.py
@@ -1,8 +1,8 @@
 import time
-def printDelay(t: str, d=1):
+def printDelay(t: str, d=0.5):
     time.sleep(d)
     print(t)
 
 for i in range(30,0,-1):
     printDelay(i) 
-printDelay('raketlancering gestart!')
+printDelay('de raketlancering is gestart!')
---
+++ Module 03 - Keer Op Keer/tijd.py
@@ -0,0 +1,6 @@
+for i in range(1,13):
+   print(f'{i} am')
+
+for i in range(1,13):
+    print(f'{i} pm')
+
