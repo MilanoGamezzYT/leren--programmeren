8d2dc4c51eefedfe54ff114b8907661a39786af1
Milan Sebes <99067157@mydavinci.nl>
2022-12-15 09:57:00 +0100

    lijstjes

--- Module 04 - Lijstjes en samenstellingen/boodschappenlijst.py
+++ Module 04 - Lijstjes en samenstellingen/boodschappenlijst.py
@@ -1,17 +1,20 @@
 lijstje = {}
-userInput = input('Wilt u wat toevoegen aan het boodschappenlijstje? ').lower()
+vraag = True
 
-while not userInput != 'ja':
-    product_naam = str(input('Wat wilt u toevoegen aan het boodschappenlijstje? ')).lower()
+while vraag:
+    userInput = input('Wilt u wat toevoegen aan het boodschappenlijstje? ja/nee  ').lower()
+    if userInput == 'nee':
+        vraag = False
+    else:
+        product_naam = str(input('Wat wil je toevoegen aan het boodschappenlijstje? ')).lower()
     hoeveelheid = int(input(f'Hoeveel wilt u toevoegen van {product_naam}? '))
     if product_naam in lijstje:
         lijstje[product_naam] += hoeveelheid
     else:
         lijstje[product_naam] = hoeveelheid
-    userInput = input('Wilt u wat toevoegen aan het boodschappenlijstje? J/N ').lower()
 
-print('- Boodschappenlijst -')
+print('-[BOODSCHAPPENLIJSTJE] -')
 print('')
 print(lijstje)
 print('')
-print('------------------------')   
+print('---------------------')   
--- Module 04 - Lijstjes en samenstellingen/kaarten.py
+++ Module 04 - Lijstjes en samenstellingen/kaarten.py
@@ -1,17 +1,17 @@
 import random
 
-KaartTypes = ("harten ", "klaveren ", "schoppen ", "ruiten ")
-Nummers = ("aas", "2", "3", "4", "5", "6", "7", "8", "9", "10", "boer", "vrouw", "koning")
-Deck = ["joker", "joker"]
+kaarten = ('ruiten ', 'schoppen ', 'klaveren ', 'harten')
+cijfers = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'boer', 'vrouw', 'koning', 'aas')
+deck = ['joker', 'joker']
 
-for x in (KaartTypes):
-    for y in (Nummers):
-        optellen = x + y 
-        Deck.append(optellen)
+for m in (kaarten):
+    for s in (cijfers):
+        optellen = m + s
+        deck.append(optellen)
+random.shuffle(deck)
 
-random.shuffle(Deck)
-for z in range(1,8):
-    print(f'Kaart {z}: {Deck[z]}')
-    Deck.pop(z)
+for g in range(1,8):
+    print(f'Kaart {g}: {deck[g]}')
+    deck.pop(g)
 
-print(f"deck {len(Deck)} kaarten: {Deck}")
+print(f"deck ({len(deck)} kaarten): {deck}")
