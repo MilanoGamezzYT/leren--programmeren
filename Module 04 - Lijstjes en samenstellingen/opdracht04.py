7470edc5b5ad50f9b891d30bb87dab9e8d57e7ff
Milan Sebes <99067157@mydavinci.nl>
2022-12-16 10:22:44 +0100

    fruitmand

--- Module 04 - Lijstjes en samenstellingen/opdracht04.py
+++ Module 04 - Lijstjes en samenstellingen/opdracht04.py
@@ -1,7 +1,7 @@
 from fruitmand import fruitmand
 import random
 
-vraag = int(input('Hoeveel fruit wil je toevoegen? '))
-for x in range(vraag):
+hoeveelheid = int(input('Hoeveel fruit wil je toevoegen? '))
+for x in range(hoeveelheid):
     fruit_naam = random.choice(fruitmand)
     print(fruit_naam['name'])
--- Module 04 - Lijstjes en samenstellingen/opdracht05.py
+++ Module 04 - Lijstjes en samenstellingen/opdracht05.py
@@ -3,15 +3,3 @@ from fruitmand import fruitmand
 fruitmand1 = reversed(fruitmand)
 for x in fruitmand1:
     print(x['name'])
-
-
-
-
-
-
-
-
-
-
-
-
