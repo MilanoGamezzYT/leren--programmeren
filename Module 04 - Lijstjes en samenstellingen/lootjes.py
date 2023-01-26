6d2709fb147eabeaf6efdaeebe16f164e6b4872e
Milan Sebes <99067157@mydavinci.nl>
2023-01-26 09:34:17 +0100

    Lootjes

--- Module 04 - Lijstjes en samenstellingen/lootjes.py
+++ Module 04 - Lijstjes en samenstellingen/lootjes.py
@@ -1,13 +1,32 @@
 # lootjes verdeler met 3 of meer personen
 
-from random import randint
-naam = input("Geef je naam op: ")
-personen = [naam]
-while True:
-    naam = input("Geef een naam op: ")
-    if naam == "":
-        break
-    else:
-        personen.append(naam)
-print(personen)
+from random import shuffle
+lijst1 = []
+lijst2 = []
+stop = False
+stop2 = False
 
+while not stop:
+    userinput = input('geef een naam voor op het lootje  ')
+    if userinput not in lijst1:
+        lijst1.append(userinput)
+        lijst2.append(userinput)
+    else:
+        print('deze naam is al opgegeven')
+
+    if len(lijst1) >= 3:
+        nog_een_naam = input('wil je nog een naam toevoegen?  ').lower()
+        if nog_een_naam == 'nee':
+           stop = True
+
+shuffle(lijst1)
+
+
+while not stop2:
+    shuffle(lijst1)
+    stop2 = True
+    for x in range(0,len(lijst2)):
+        if lijst1[x] == lijst2[x]:
+            stop2 = False
+for x in range(0,len(lijst2)):
+    print(f'{lijst1[x]} heeft {lijst2[x]} getrokken')
