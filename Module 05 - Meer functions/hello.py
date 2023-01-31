fd96248aac28eaffd4ae96df955ec4237ba93c27
Milan Sebes <99067157@mydavinci.nl>
2023-01-31 09:10:55 +0100

    functions

---
+++ Module 05 - Meer functions/hello.py
@@ -0,0 +1,15 @@
+# def string() -> str:
+#     return "Hello from function town"
+
+# print(string())
+
+
+def towns(amount: int):
+    for x in range(1, amount + 1):
+        print(f"Hello from function town {x}!")
+    print()
+
+
+towns(3)
+
+towns(7)
