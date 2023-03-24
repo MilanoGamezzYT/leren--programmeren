b78c731cd31c3c5bae48a56fb841bd25f537e8f5
MilanoGamezzYT <99067157@mydavinci.nl>
2023-03-24 10:22:58 +0100

    IjsSalon

--- Module 05 - Meer functions/Ijssalon/IjsSalon.py
+++ Module 05 - Meer functions/Ijssalon/IjsSalon.py
@@ -1,6 +1,19 @@
 # maak een dry ijssalon met behulp van de flowchart
-start = int(input("Welkom bij papi Gelato, hoeveel bolletjes wilt u bestellen?  "))
-if start == "1" or "2" or "3":
-    input("Prima, wilt uw een bakje of een hoorntje?")
-elif start == "4" or "5" or "6" or "7" or "8":
-    input("Prima, u krijgt een bakje")
+
+aantal = input("Welkom bij papi Gelato, hoeveel bolletjes wilt u bestellen?  ")
+if aantal == "1" or "2" or "3":
+    keuze = input("Prima, wilt uw een bakje of een hoorntje?")
+    if keuze == "bakje":
+        print(f'hier is uw {keuze} met {aantal} bolletje(s)')
+    elif keuze == "hoorntje":
+        print(f'hier is uw {keuze} met {aantal} bolletje(s)')
+        
+    else:
+        print("sorry, dat snap ik niet..")
+
+elif aantal == "4" or "5" or "6" or "7" or "8":
+    print("Prima, u krijgt een bakje")
+elif aantal > 8:
+    print("Sorry, zulke grote bakken hebben we niet")
+else: 
+    print("Sorry, dat snap ik niet...")
