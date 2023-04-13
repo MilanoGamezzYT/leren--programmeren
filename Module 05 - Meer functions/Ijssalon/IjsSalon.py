48997e26dcdeb82bb408a97aa648cf2fff561390
MilanoGamezzYT <99067157@mydavinci.nl>
2023-04-13 16:09:25 +0200

    python

--- Module 05 - Meer functions/Ijssalon/IjsSalon.py
+++ Module 05 - Meer functions/Ijssalon/IjsSalon.py
@@ -1,4 +1,8 @@
-welkom = print('Welkom bijPapi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.')
-aantal = int(input('Hoeveel bolletjes wilt u?'))
-if aantal == (1,3):
-    vorm = input(f'“Wilt u deze{aantal} bolletje(s) in een hoorntje of een bakje?”')
+welkom = print('Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.' )
+aantal = int(input('Hoeveel bolletjes wilt u? '))
+if aantal == 1:
+    vorm = input(f'Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje?' )
+    keuze = print(f'Hier is uw {vorm} met {aantal} bolletje(s).')
+    meer = input('Wilt u nog meer bestellen?' )
+    if meer == 'nee':
+        print('Bedankt en tot ziens!')
